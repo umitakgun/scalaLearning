27456bebb19dfd9f020bb719d98d7a4109e2de67
Umit Akgun <i.umitakgun@gmail.com>
2016-01-04 18:28:30 -0600

    week 1

--- src/main/scala/example/Knuth.scala
+++ src/main/scala/example/Knuth.scala
@@ -4,6 +4,28 @@ package example
   * Created by umit on 03.01.2016.
   */
 object Knuth extends App {
-  let t = 5
-  println("Hello World")
+  def t = 5
+
+  def abs(x: Double) = if (x<0) -x else x
+
+  def sqrtIter(guess: Double, x: Double) : Double =
+    if (isGoodEnough(guess, x)) guess
+    else sqrtIter(improve(guess,x), x)
+
+  def isGoodEnough(guess: Double, x: Double) =
+    abs(guess*guess - x) / x < 0.001
+
+  def improve(guess: Double, x: Double) =
+    (guess + x / guess) / 2
+
+  def sqrt(x: Double) = sqrtIter(1.0, x)
+
+  println(sqrt(1.0e50))
+
+//  0.001
+//  0.1e-20
+//  1.0e20
+//  1.0e50
+//  have to write is good enough
+
 }
--- src/main/scala/turing.sc
+++ src/main/scala/turing.sc
@@ -3,3 +3,29 @@
 val x = 1
 def increase(i: Int) = i + 1
 increase(x)
+
+def square(x: Double) = x * x
+
+square(2)
+
+square(4+5)
+
+//def power(x: Double, y: Int) : Double
+
+def loop: Boolean = loop
+
+def constOne(x: Int, y: => Int) : Int = 1
+
+def loopInt: Int = loopInt
+
+constOne(3,loopInt)
+
+def and(x: Boolean, y: => Boolean) : Boolean = if (x) y else false
+
+def or(x: Boolean, y: => Boolean) : Boolean = if (x) true else y
+
+and(true,false)
+and(false, loop)
+
+or(true,false)
+or(false,false)
